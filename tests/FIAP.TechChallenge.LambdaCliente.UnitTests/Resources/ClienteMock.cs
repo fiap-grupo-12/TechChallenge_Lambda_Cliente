4abbf15fc7f723f238231f7837dd72988d0acd55
nataliamld <natalia.maldaner@outlook.com>
2024-12-01 18:47:02 -0300

    update unitTests

--- tests/FIAP.TechChallenge.LambdaCliente.UnitTests/Resources/ClienteMock.cs
+++ tests/FIAP.TechChallenge.LambdaCliente.UnitTests/Resources/ClienteMock.cs
@@ -1,4 +1,5 @@
-﻿using FIAP.TechChallenge.LambdaCliente.Domain.Entities;
+﻿using FIAP.TechChallenge.LambdaCliente.Application.Models.Response;
+using FIAP.TechChallenge.LambdaCliente.Domain.Entities;
 
 namespace FIAP.TechChallenge.LambdaCliente.UnitTests.Resources
 {
@@ -14,6 +15,24 @@ namespace FIAP.TechChallenge.LambdaCliente.UnitTests.Resources
                 Email = "teste@teste.com"
             },
             new Cliente
+            {
+                Id = new Guid(),
+                Nome = "Client Teste 2",
+                CPF = "13272968024",
+                Email = "teste@teste.com"
+            }
+        ];
+
+        public static List<ClienteResponse> GetClienteResponseMock() =>
+        [
+            new ClienteResponse
+            {
+                Id = new Guid(),
+                Nome = "Client Teste",
+                CPF = "10927749041",
+                Email = "teste@teste.com"
+            },
+            new ClienteResponse
             {
                 Id = new Guid(),
                 Nome = "Client Teste 2",
--- tests/FIAP.TechChallenge.LambdaCliente.UnitTests/UseCases/CriarClienteUseCaseTests.cs
+++ tests/FIAP.TechChallenge.LambdaCliente.UnitTests/UseCases/CriarClienteUseCaseTests.cs
@@ -1,6 +1,53 @@
-﻿namespace FIAP.TechChallenge.LambdaCliente.UnitTests.UseCases
+﻿using AutoMapper;
+using FIAP.TechChallenge.LambdaCliente.Application.UseCases.Interfaces;
+using FIAP.TechChallenge.LambdaCliente.Application.UseCases;
+using FIAP.TechChallenge.LambdaCliente.Domain.Repositories;
+using FIAP.TechChallenge.LambdaCliente.UnitTests.Resources;
+using Moq;
+using Xunit;
+using FIAP.TechChallenge.LambdaCliente.Infra.Configurations;
+using FIAP.TechChallenge.LambdaCliente.Domain.Entities;
+using FluentAssertions;
+using FIAP.TechChallenge.LambdaCliente.Api.Request;
+
+namespace FIAP.TechChallenge.LambdaCliente.UnitTests.UseCases
 {
     public class CriarClienteUseCaseTests
     {
+        private readonly Mock<IClienteRepository> _clienteRepositoryMock;
+        private readonly Mock<IObterClientesUseCase> _useCase;
+        private readonly IMapper _mapper;
+
+        public CriarClienteUseCaseTests()
+        {
+            _clienteRepositoryMock = new Mock<IClienteRepository>();
+            _mapper = new MapperConfiguration(c => c.AddProfile<MapperConfig>()).CreateMapper();
+        }
+
+        [Fact]
+        public async void ObterTodosOsClientes_WithSuccess()
+        {
+            var cliente = new CriarClienteRequest
+            {
+                Nome = "Client Teste",
+                CPF = "10927749041",
+                Email = "teste@teste.com"
+            };
+
+            var clienteMock = ClienteMock.GetClienteMock().First();
+
+            var clienteResponse = ClienteMock.GetClienteResponseMock().First();
+
+            _clienteRepositoryMock
+                .Setup(x => x.Post(It.IsAny<Cliente>()))
+                .ReturnsAsync(clienteMock);
+
+            var useCase = new CriarClienteUseCase(_clienteRepositoryMock.Object, _mapper);
+
+            var result = await useCase.Execute(cliente.ToInput());
+
+            Assert.NotNull(result);
+            result.Should().BeEquivalentTo(clienteResponse);
+        }
     }
 }

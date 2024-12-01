968c4324031074da279053a55b0437aa8fd43b7e
nataliamld <natalia.maldaner@outlook.com>
2024-12-01 13:58:43 -0300

    update table

--- src/FIAP.TechChallenge.LambdaCliente.Domain/Entities/Cliente.cs
+++ src/FIAP.TechChallenge.LambdaCliente.Domain/Entities/Cliente.cs
@@ -1,9 +1,15 @@
 ﻿using Amazon.DynamoDBv2.DataModel;
+using System.Diagnostics.CodeAnalysis;
 
 namespace FIAP.TechChallenge.LambdaCliente.Domain.Entities;
 
+[ExcludeFromCodeCoverage]
+[DynamoDBTable("ClienteTable")]
 public class Cliente : EntityBase
 {
+    //[DynamoDBHashKey("id")]
+    //public Guid Id { get; set; }
+
     [DynamoDBProperty("nome")]
     public string Nome { get; set; }
 

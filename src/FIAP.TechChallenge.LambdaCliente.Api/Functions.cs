162a566d45f66ad2c2d96e017c9e8dd2529be874
nataliamld <natalia.maldaner@outlook.com>
2024-12-01 15:20:30 -0300

    refac

--- src/FIAP.TechChallenge.LambdaCliente.Api/Functions.cs
+++ src/FIAP.TechChallenge.LambdaCliente.Api/Functions.cs
@@ -22,26 +22,36 @@ public class Function(ICriarClienteUseCase criarClienteUseCase,
     private readonly IObterClientesUseCase _obterClientesUseCase = obterClienteUseCase;
     private readonly IObterClientePorCpfUseCase _obterClientePorCpfUseCase = obterClientePorCpfUseCase;
 
-    //[LambdaFunction(ResourceName = "Handler")]
-    //public async Task<APIGatewayProxyResponse?> Handler(APIGatewayProxyRequest request, ILambdaContext context)
-    //{
-    //    bool methodOk = false;
-    //    List<object> parameters = [];
-
-    //    LambdaHttpMethod httpMethod = Enum.Parse<LambdaHttpMethod>(request.HttpMethod, true);
-    //    return await GetResponseAsync(request,
-    //                                  methodOk,
-    //                                  parameters,
-    //                                  httpMethod);
-    //}
-
     [LambdaFunction(ResourceName = "Handler")]
-    public async Task<APIGatewayProxyResponse> Handler(APIGatewayProxyRequest request, ILambdaContext context)
+    public async Task<APIGatewayProxyResponse?> Handler(APIGatewayProxyRequest request, ILambdaContext context)
     {
         bool methodOk = false;
-        List<object> parameters = new List<object>();
+        List<object> parameters = [];
 
         LambdaHttpMethod httpMethod = Enum.Parse<LambdaHttpMethod>(request.HttpMethod, true);
+        return await GetResponseAsync(request,
+                                      methodOk,
+                                      parameters,
+                                      httpMethod);
+    }
+
+    [HttpApi(LambdaHttpMethod.Get, "/Cliente/{cpf}")]
+    public async Task<ClienteResponse> ObterClientePorCpf(string cpf)
+        => await _obterClientePorCpfUseCase.Execute(cpf);
+
+    [HttpApi(LambdaHttpMethod.Get, "/Cliente")]
+    public async Task<IList<ClienteResponse>> ObterTodosOsClientes()
+        => await _obterClientesUseCase.Execute();
+
+    [HttpApi(LambdaHttpMethod.Post, "/Cliente")]
+    public async Task<ClienteResponse> CriarClientes([FromBody] CriarClienteRequest request)
+        => await _criarClienteUseCase.Execute(request.ToInput());
+
+    private async Task<APIGatewayProxyResponse?> GetResponseAsync(APIGatewayProxyRequest request,
+        bool methodOk,
+        List<object> parameters,
+        LambdaHttpMethod httpMethod)
+    {
         try
         {
             foreach (var method in this.GetType().GetMethods().Where(x => x.Name != "Handler"))
@@ -88,73 +98,7 @@ public class Function(ICriarClienteUseCase criarClienteUseCase,
                 Headers = new Dictionary<string, string> { { "Content-Type", "application/json" } }
             };
         }
+
         return null;
     }
-
-    [HttpApi(LambdaHttpMethod.Get, "/Cliente/{cpf}")]
-    public async Task<ClienteResponse> ObterClientePorCpf(string cpf)
-        => await _obterClientePorCpfUseCase.Execute(cpf);
-
-    [HttpApi(LambdaHttpMethod.Get, "/Cliente")]
-    public async Task<IList<ClienteResponse>> ObterTodosOsClientes()
-        => await _obterClientesUseCase.Execute();
-
-    [HttpApi(LambdaHttpMethod.Post, "/Cliente")]
-    public async Task<ClienteResponse> CriarClientes([FromBody] CriarClienteRequest request)
-        => await _criarClienteUseCase.Execute(request.ToInput());
-
-    //private async Task<APIGatewayProxyResponse?> GetResponseAsync(APIGatewayProxyRequest request,
-    //    bool methodOk,
-    //    List<object> parameters,
-    //    LambdaHttpMethod httpMethod)
-    //{
-    //    try
-    //    {
-    //        foreach (var method in this.GetType().GetMethods().Where(x => x.Name != "Handler"))
-    //        {
-    //            foreach (var attributes in method.CustomAttributes.Where(x => x.ConstructorArguments.Count > 1))
-    //            {
-    //                int methodType = (int)attributes.ConstructorArguments.FirstOrDefault(x => x.ArgumentType.Name == "LambdaHttpMethod").Value;
-    //                var pathType = attributes.ConstructorArguments.FirstOrDefault(x => x.ArgumentType.Name == "String").Value.ToString();
-
-    //                methodOk = httpMethod == (LambdaHttpMethod)methodType && string.Equals(pathType, request.Resource, StringComparison.CurrentCultureIgnoreCase);
-    //            }
-    //            if (methodOk)
-    //            {
-    //                foreach (var parameter in method.GetParameters())
-    //                    if (parameter.CustomAttributes.Count() > 0)
-    //                        parameters.Add(Newtonsoft.Json.JsonConvert.DeserializeObject(request.Body, Type.GetType(parameter.ParameterType.AssemblyQualifiedName)));
-    //                    else
-    //                        foreach (var stringParameters in request.PathParameters.Where(x => x.Key == parameter.Name))
-    //                            parameters.Add(stringParameters.Value);
-
-    //                var resultAsync = method.Invoke(this, [.. parameters]);
-
-    //                if (resultAsync is Task task)
-    //                {
-    //                    await task;
-    //                    var resultProperty = task.GetType().GetProperty("Result");
-
-    //                    return new APIGatewayProxyResponse
-    //                    {
-    //                        StatusCode = 200,
-    //                        Body = Newtonsoft.Json.JsonConvert.SerializeObject(resultProperty?.GetValue(task)),
-    //                        Headers = new Dictionary<string, string> { { "Content-Type", "application/json" } }
-    //                    };
-    //                }
-    //            }
-    //        }
-    //    }
-    //    catch (Exception ex)
-    //    {
-    //        return new APIGatewayProxyResponse
-    //        {
-    //            StatusCode = 400,
-    //            Body = ex.Message,
-    //            Headers = new Dictionary<string, string> { { "Content-Type", "application/json" } }
-    //        };
-    //    }
-
-    //    return null;
-    //}
 }
